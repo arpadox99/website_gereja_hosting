e03cad72a5e889dc2c664d948ef658bcd7e89f73
GabZ <141195666+arpadox99@users.noreply.github.com>
2024-06-27 19:13:17 +0900

    fix path

--- content/media.php
+++ content/media.php
@@ -463,10 +463,10 @@
             <img src="../img/media/BA/1.jpg" class="mx-auto d-block img-fluid w-100" alt="baptisan air">
           </div>
           <div class="carousel-item" data-bs-interval="5000">
-            <img src="/img/media/BA/2.jpg" class="mx-auto d-block img-fluid w-100" alt="...">
+            <img src="/img/media/BA/2.jpg" class="mx-auto d-block img-fluid w-100" alt="gambar2">
           </div>
           <div class="carousel-item" data-bs-interval="5000">
-            <img src="../img/media/BA/3.jpg" class="mx-auto d-block img-fluid w-100" alt="...">
+            <img src="../img/media/BA/3.jpg" class="mx-auto d-block img-fluid w-100" alt="gmbr99">
           </div>
           <div class="carousel-item" data-bs-interval="5000">
             <img src="../img/media/BA/4.jpg" class="mx-auto d-block img-fluid w-100" alt="...">
