03bc57155de04b97a38c604bb11d626656d5a9e8
GabZ <141195666+arpadox99@users.noreply.github.com>
2024-07-26 01:03:29 +0900

    26 JULY UPDATE

--- admin/jadwalsimpan.php
+++ admin/jadwalsimpan.php
@@ -1,39 +1,45 @@
 <?php
 
+$id_jadwal = filter_var($_POST['id_jadwal'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $hari_tgl = filter_var($_POST['hari_tgl'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $waktu_ibadah = filter_var($_POST['waktu_ibadah'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $jenis_keg = filter_var($_POST['jenis_keg'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $lokasi_ibadah = filter_var($_POST['lokasi_ibadah'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-if (empty($hari_tgl) || empty($waktu_ibadah) || empty($jenis_keg) || empty($lokasi_ibadah)) {
-
+if (empty($id_jadwal) || empty($hari_tgl) || empty($waktu_ibadah) || empty($jenis_keg) || empty($lokasi_ibadah)) {
   echo "<script>
           alert('Data Tidak Boleh Kosong');
           window.location.href = 'index.php?page=jadwal';
       </script>";
 } else {
-  $cek = $con->prepare("SELECT * FROM jadwal_ibadah WHERE hari_tgl = ? ");
-  $cek->bindParam(1, $hari_tgl);
-  $cek->execute();
-  $jumlah = $cek->rowCount();
+  try {
+    $stmt = $con->prepare("SELECT * FROM jadwal_ibadah WHERE id_jadwal = ?");
+    $stmt->bindValue(1, $id_jadwal);
+    $stmt->execute();
+    $jumlah = $stmt->rowCount();
 
-  if ($jumlah > 0) {
+    if ($jumlah > 0) {
+      echo "<script>
+              alert('Data Sudah Ada');
+              window.location.href = 'index.php?page=jadwal';
+          </script>";
+    } else {
+      $sql = "INSERT INTO jadwal_ibadah (hari_tgl, waktu_ibadah, jenis_keg, lokasi_ibadah) VALUES (:hari_tgl, :waktu_ibadah, :jenis_keg, :lokasi_ibadah)";
+      $simpan = $con->prepare($sql);
+      $simpan->bindValue(':hari_tgl', $hari_tgl);
+      $simpan->bindValue(':waktu_ibadah', $waktu_ibadah);
+      $simpan->bindValue(':jenis_keg', $jenis_keg);
+      $simpan->bindValue(':lokasi_ibadah', $lokasi_ibadah);
+      $simpan->execute();
 
+      echo "<script>
+              alert('Data Berhasil Disimpan');
+              window.location.href = 'index.php?page=jadwal';
+          </script>";
+    }
+  } catch (PDOException $e) {
     echo "<script>
-            alert('Data Sudah Ada');
-             window.location.href = 'index.php?page=jadwal';
-        </script>";
-  } else {
-    $sql = "INSERT INTO jadwal_ibadah (hari_tgl, waktu_ibadah, jenis_keg, lokasi_ibadah) VALUES (:hari_tgl, :waktu_ibadah, :jenis_keg, :lokasi_ibadah)";
-    $simpan = $con->prepare($sql);
-    $simpan->bindParam('hari_tgl', $hari_tgl);
-    $simpan->bindParam('waktu_ibadah', $waktu_ibadah);
-    $simpan->bindParam('jenis_keg', $jenis_keg);
-    $simpan->bindParam('lokasi_ibadah', $lokasi_ibadah);
-    $simpan->execute();
-
-    echo "<script>
-            alert('Data Berhasil Disimpan');
+            alert('Terjadi kesalahan: " . $e->getMessage() . "');
             window.location.href = 'index.php?page=jadwal';
         </script>";
   }
