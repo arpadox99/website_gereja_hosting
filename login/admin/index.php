f062995e7668fea0a21c5589b53f08d9bdcf6fcd
GabZ <141195666+arpadox99@users.noreply.github.com>
2024-07-26 22:11:54 +0900

    26 JULY UPDATE

--- login/admin/index.php
+++ login/admin/index.php
@@ -59,7 +59,7 @@ if (empty($_SESSION['user-websitegereja'])) {
                         <div class="nav">
                             <div class="sb-sidenav-menu-heading"> CORE </div>
                             <a class="nav-link" href="index.php">
-                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                <div class="sb-nav-link-icon"><i class="fa-solid fa-cross"></i></div>
                                 Dashboard
                             </a>
                             <div class="sb-sidenav-menu-heading"> MENU </div>
