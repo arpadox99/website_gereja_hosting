1677f4a53765722cb0de57e16075344ffc031c24
GabZ <141195666+arpadox99@users.noreply.github.com>
2024-07-26 21:31:48 +0900

    26 JULY UPDATE

--- login/admin/admin.php
+++ login/admin/admin.php
@@ -9,7 +9,7 @@
 
 <body>
   <div class="container-fluid px-4">
-    <h1 class="mt-4"> Dashboard <span> <i class="fas-light fa-cross"></i> </span></h1>
+    <h1 class="mt-4"> Dashboard <span> <i class="fa-solid fa-cross"></i> </span></h1>
     <ol class="breadcrumb mb-4">
       <li class="breadcrumb-item active"> Tampilan Website Gereja GBI GOD'S GRACE </li>
     </ol>
