8007ca4bb1f89bc1f9e92d40e2dc344011d6299b
GabZ <141195666+arpadox99@users.noreply.github.com>
2024-07-28 23:39:22 +0900

    28 JULY UPDATE

--- login/admin/index.php
+++ login/admin/index.php
@@ -25,7 +25,37 @@ if (empty($_SESSION['user-websitegereja'])) {
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
         <link rel="icon" href="../img/Logo/gbi.png" />
+        <style>
+            html,
+            body {
+                height: 100%;
+                margin: 0;
+            }
 
+            #layoutSidenav {
+                display: flex;
+                height: 100%;
+            }
+
+            #layoutSidenav_content {
+                flex: 1;
+                display: flex;
+                flex-direction: column;
+            }
+
+            main {
+                flex: 1;
+            }
+
+            footer {
+                position: fixed;
+                bottom: 0;
+                width: calc(100% - 225px);
+                /* Adjust width based on sidenav width */
+                left: 225px;
+                /* Match the sidenav width */
+            }
+        </style>
     </head>
 
     <body class="sb-nav-fixed">
@@ -141,6 +171,7 @@ if (empty($_SESSION['user-websitegereja'])) {
                 </footer>
             </div>
         </div>
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="js/scripts.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
