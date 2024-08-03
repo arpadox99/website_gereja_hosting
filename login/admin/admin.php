bf91eadd3ff7760fd525b9f705ec2d70ed30a64f
GabZ <141195666+arpadox99@users.noreply.github.com>
2024-08-03 14:13:01 +0900

    03 AUGUST UPDATE

--- login/admin/admin.php
+++ login/admin/admin.php
@@ -13,7 +13,7 @@
     <ol class="breadcrumb mb-4">
       <li class="breadcrumb-item active"> Tampilan Website Gereja GBI GOD'S GRACE </li>
     </ol>
-    <iframe src="../../index.php" width="1285px" height="500"></iframe>
+    <iframe src="projectwebsitegereja.my.id" width="1285px" height="500"></iframe>
   </div>
 </body>
 
