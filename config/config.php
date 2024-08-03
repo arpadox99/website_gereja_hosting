b86445b07554301d5ed079413e32eeb94981a747
GabZ <141195666+arpadox99@users.noreply.github.com>
2024-08-03 13:31:15 +0900

    03 AUGUST UPDATE

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 <?php
 try {
     //koneksi..
-    $con = new PDO('mysql:host=localhost;dbname=u152600978_websitegereja', "u152600978_website_gereja", "");
+    $con = new PDO('mysql:host=localhost; dbname=u152600978_websitegereja', "u152600978_website_gereja", "G0dsGrace33");
     //set.eror
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
