720f029bf8dd8d83ef360ec185cd29a838eea53c
GabZ <141195666+arpadox99@users.noreply.github.com>
2024-08-03 13:34:22 +0900

    03 AUGUST UPDATE

--- config/config.php
+++ config/config.php
@@ -1,10 +1,13 @@
-<!-- CONFIG UNTUK LOCALHOST -->
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 try {
     //koneksi..
-    $con = new PDO('mysql:host=127.0.0.1; dbname=u152600978_websitegereja', "u152600978_website_gereja", "G0dsGrace33");
+    $con = new PDO('mysql:host=localhost;dbname=u152600978_websitegereja', "u152600978_website_gereja", "G0dsgrace33");
     //set.eror
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Koneksi berhasil";
 } catch (Exception $e) {
     //jika.ada.eror
     echo "Koneksi Gagal : " . $e->getMessage() . "<br>";
