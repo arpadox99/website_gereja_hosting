8d3ba467af180d4479729cace967f3a23678b21a
GabZ <141195666+arpadox99@users.noreply.github.com>
2024-08-05 05:29:49 +0900

    05 AUGUST UPDATE

--- login/admin/admin.php
+++ login/admin/admin.php
@@ -7,7 +7,8 @@
   <link rel="stylesheet" href="../admin/css/admin.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <style>
-    body, html {
+    body,
+    html {
       height: 100%;
       margin: 0;
       display: flex;
@@ -23,14 +24,15 @@
     .content {
       flex: 1;
       overflow: auto;
-      padding-bottom: 60px; /* Height of the footer */
+      padding-bottom: 60px;
+      /* Height of the footer */
     }
 
     .iframe-container {
       position: relative;
       width: 100%;
-      height: 0;
-      padding-bottom: 56.25%; /* 16:9 Aspect Ratio */
+      flex: 1;
+      overflow: hidden;
     }
 
     .iframe-container iframe {
@@ -47,6 +49,7 @@
       bottom: 0;
       width: 100%;
       padding: 10px;
+      background-color: darkgrey;
     }
   </style>
 </head>
@@ -63,11 +66,34 @@
       </div>
     </div>
   </div>
+  <footer class="py-4 bg-dark mt-auto">
+    <div class="container-fluid px-1">
+      <div class="d-flex align-items-center justify-content-between small">
+        <div class="text-muted">
+          <small>
+            &copy; <script>
+              document.write(new Date().getFullYear());
+            </script> GOD'S GRACE
+          </small>
+        </div>
+        <div>
+          <a target="_blank" class="socialfooter" href="https://www.facebook.com/church.grace.940" title="Facebook GBI">
+            <img draggable="false" src="../img/Logo/facebook.png" width="30" height="30" alt="FB">
+          </a>
+          <a target="_blank" class="socialfooter" href="https://www.instagram.com/gbigodsgrace" title="Instagram GBI">
+            <img draggable="false" src="../img/Logo/instagram.png" width="30" height="30" alt="IG">
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
   <script>
     function adjustIframeHeight() {
       const iframe = document.getElementById('iframe');
-      const containerHeight = window.innerHeight - document.querySelector('footer').offsetHeight - document.querySelector('.content').offsetTop;
-      iframe.style.height = containerHeight + 'px';
+      const contentHeight = document.querySelector('.content').offsetHeight;
+      const footerHeight = document.querySelector('footer').offsetHeight;
+      const containerHeight = window.innerHeight - footerHeight - document.querySelector('.content').offsetTop;
+      iframe.style.height = (containerHeight > contentHeight ? contentHeight : containerHeight) + 'px';
     }
 
     window.addEventListener('resize', adjustIframeHeight);
