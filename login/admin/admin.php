4a0b4eaeedf51a706bbb45f2edfb6d309c335916
GabZ <141195666+arpadox99@users.noreply.github.com>
2024-08-05 05:00:48 +0900

    05 AUGUST UPDATE

--- login/admin/admin.php
+++ login/admin/admin.php
@@ -5,6 +5,24 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="../admin/css/admin.css">
+  <style>
+    .iframe-container {
+      position: relative;
+      width: 100%;
+      overflow: hidden;
+      padding-top: 56.25%;
+      /* 16:9 Aspect Ratio */
+    }
+
+    .iframe-container iframe {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      border: 0;
+    }
+  </style>
 </head>
 
 <body>
@@ -13,7 +31,9 @@
     <ol class="breadcrumb mb-4">
       <li class="breadcrumb-item active"> Tampilan Website Gereja GBI GOD'S GRACE </li>
     </ol>
-    <iframe src="https://projectwebsitegereja.my.id/" width="1285px" height="500"></iframe>
+    <div class="iframe-container">
+      <iframe src="https://projectwebsitegereja.my.id/" width="1285px" height="500"></iframe>
+    </div>
   </div>
 </body>
 
