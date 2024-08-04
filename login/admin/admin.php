38927a65e9fc8e2d888a6eaf4521ac3f1352225e
GabZ <141195666+arpadox99@users.noreply.github.com>
2024-08-05 05:04:51 +0900

    05 AUGUST UPDATE

--- login/admin/admin.php
+++ login/admin/admin.php
@@ -5,7 +5,25 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="../admin/css/admin.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <style>
+    body,
+    html {
+      height: 100%;
+      margin: 0;
+    }
+
+    .container-fluid {
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+    }
+
+    .content {
+      flex: 1;
+      overflow: auto;
+    }
+
     .iframe-container {
       position: relative;
       width: 100%;
@@ -19,20 +37,31 @@
       top: 0;
       left: 0;
       width: 100%;
-      height: 50%;
+      height: 100%;
       border: 0;
+    }
+
+    footer {
+      position: fixed;
+      bottom: 0;
+      width: 100%;
+      background-color: #f1f1f1;
+      text-align: center;
+      padding: 10px;
     }
   </style>
 </head>
 
 <body>
   <div class="container-fluid px-4">
-    <h1 class="mt-4"> Dashboard <span> <i class="fa-solid fa-cross"></i> </span></h1>
-    <ol class="breadcrumb mb-4">
-      <li class="breadcrumb-item active"> Tampilan Website Gereja GBI GOD'S GRACE </li>
-    </ol>
-    <div class="iframe-container">
-      <iframe src="https://projectwebsitegereja.my.id/"></iframe>
+    <div class="content">
+      <h1 class="mt-4"> Dashboard <span> <i class="fa-solid fa-cross"></i> </span></h1>
+      <ol class="breadcrumb mb-4">
+        <li class="breadcrumb-item active"> Tampilan Website Gereja GBI GOD'S GRACE </li>
+      </ol>
+      <div class="iframe-container">
+        <iframe src="https://projectwebsitegereja.my.id/"></iframe>
+      </div>
     </div>
   </div>
 </body>
