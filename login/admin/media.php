cd0b3fb1a45cbca7d0ca204bb291ecc683626d20
GabZ <141195666+arpadox99@users.noreply.github.com>
2024-08-09 10:45:16 +0900

    09 AUGUST UPDATE

--- login/admin/media.php
+++ login/admin/media.php
@@ -6,78 +6,76 @@
   </ol>
   <div class="card mb-4">
     <div class="card-header">
-      <i class="fas fa-table me-1"></i>
-      Daftar Image Slider
+        <i class="fas fa-table me-1"></i>
+        Daftar Image Slider
     </div>
     <div class="card-body">
-      <table id="datatablesSimple">
-        <div class="col-md-12 bg-light">
-          <div class="d-flex justify-content-between">
-            <!-- Button trigger modal -->
-            <a href="index.php?page=mediaadd">
-              <button type="button" class="btn btn-primary" style="margin-bottom: 15px;">
-                <i class="fas fa-plus"></i>
-                Input Media
-              </button>
-            </a>
-            <!-- Button trigger modal -->
-            <button type="button" class="btn btn-info text-light" style="margin-bottom: 15px;" data-bs-toggle="modal" data-bs-target="#exampleModal">
-              <i class="fas fa-info"></i>
-              Info Role
-            </button>
-          </div>
-        </div>
+        <table id="datatablesSimple">
+            <div class="col-md-12 bg-light">
+                <div class="d-flex justify-content-between">
+                    <!-- Button trigger modal -->
+                    <a href="index.php?page=mediaadd">
+                        <button type="button" class="btn btn-primary" style="margin-bottom: 15px;">
+                            <i class="fas fa-plus"></i>
+                            Input Media
+                        </button>
+                    </a>
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-info text-light" style="margin-bottom: 15px;" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        <i class="fas fa-info"></i>
+                        Info Role
+                    </button>
+                </div>
+            </div>
 
-        <thead>
-          <tr>
-            <th> NO </th>
-            <th style="width: 100px;"> GAMBAR </th> <!-- Set the width of the image column -->
-            <th> NAMA GAMBAR </th>
-            <th> JUDUL </th>
-            <th> DESKRIPSI </th>
-            <th> ROLE </th>
-            <th> EDIT </th>
-            <th> DELETE </th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php
-          $no = 1;
-          $sql = $con->query("SELECT * FROM slider ORDER BY role ASC");
-          echo "<pre>";
-            var_dump($sql->fetchAll());
-          echo "</pre>";
-          while ($r = $sql->fetch()) {
-            $role = htmlspecialchars($r['role']);
-            $gambar_slider = htmlspecialchars($r['gambar_slider']);
+            <thead>
+                <tr>
+                    <th> NO </th>
+                    <th style="width: 100px;"> GAMBAR </th> <!-- Set the width of the image column -->
+                    <th> NAMA GAMBAR </th>
+                    <th> JUDUL </th>
+                    <th> DESKRIPSI </th>
+                    <th> ROLE </th>
+                    <th> EDIT </th>
+                    <th> DELETE </th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $no = 1;
+                $sql = $con->query("SELECT * FROM slider ORDER BY role ASC");
+                while ($r = $sql->fetch()) {
+                    $role = htmlspecialchars($r['role']);
+                    $gambar_slider = htmlspecialchars($r['gambar_slider']);
 
-            // Menentukan path gambar berdasarkan role
-            $gambar_path = "../img/Media/$role/$gambar_slider";
+                    // Menentukan path gambar berdasarkan role
+                    $gambar_path = "../img/Media/$role/$gambar_slider";
 
-            echo "
-                  <tr>
-                    <td>" . htmlspecialchars($no++) . "</td>
-                    <td style='width: 100px;'> <!-- Ensure the image column has a fixed width -->
-                      <img src='$gambar_path' class='img-fluid' style='max-width: 250px;'> <!-- Responsive image -->
-                    </td>
-                    <td>" . $gambar_slider . "</td>
-                    <td>" . htmlspecialchars($r['judul_slider']) . "</td>
-                    <td>" . htmlspecialchars($r['deskripsi_slider']) . "</td>
-                    <td>" . $role . "</td>
-                    <td>
-                      <a class='btn btn-warning btn-sm text-white' href='index.php?page=mediaedit&id_gambar=" . htmlspecialchars($r['id_gambar']) . "'><i class='fas fa-pencil'></i></a>
-                    </td>
-                    <td>
-                      <a class='btn btn-danger btn-sm' href='index.php?page=mediadelete&id_gambar=" . htmlspecialchars($r['id_gambar']) . "' onclick=\"return confirm('Hapus Data?')\"><i class='fas fa-trash'></i></a>
-                    </td>
-                  </tr>
-                ";
-          }
-          ?>
-        </tbody>
-      </table>
+                    echo "
+                        <tr>
+                            <td>" . htmlspecialchars($no++) . "</td>
+                            <td style='width: 100px;'> <!-- Ensure the image column has a fixed width -->
+                                <img src='$gambar_path' class='img-fluid' style='max-width: 250px;'> <!-- Responsive image -->
+                            </td>
+                            <td>" . $gambar_slider . "</td>
+                            <td>" . htmlspecialchars($r['judul_slider']) . "</td>
+                            <td>" . htmlspecialchars($r['deskripsi_slider']) . "</td>
+                            <td>" . $role . "</td>
+                            <td>
+                                <a class='btn btn-warning btn-sm text-white' href='index.php?page=mediaedit&id_gambar=" . htmlspecialchars($r['id_gambar']) . "'><i class='fas fa-pencil'></i></a>
+                            </td>
+                            <td>
+                                <a class='btn btn-danger btn-sm' href='index.php?page=mediadelete&id_gambar=" . htmlspecialchars($r['id_gambar']) . "' onclick=\"return confirm('Hapus Data?')\"><i class='fas fa-trash'></i></a>
+                            </td>
+                        </tr>
+                    ";
+                }
+                ?>
+            </tbody>
+        </table>
     </div>
-  </div>
+</div>
+
 </div>
 <!-- Modal Input -->
 <div class="modal fade" id="formJadwal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
